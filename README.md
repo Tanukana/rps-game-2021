6bb5339a3897cc208352dea161cc0c4aa8f1009c
Tony Wu <bobabeach2@gmail.com>
2021-06-02 14:14:54 -0700

    adding basic files for the game

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1>The Odin Project - RPS game from scratch</h1>
---
+++ rps.js
@@ -0,0 +1,16 @@
+function computerPlay() {
+
+}
+
+function playerSelection() {
+
+}
+
+function startGame(playerSelection, computerSelection) {
+
+}
+
+// Not exactly sure if this is what we need yet ^^
+// So we are going to " White board " or pseudo-code our assignemnt
+// before committing any further and if needed we will delete everything and start
+// from scratch.
